15c04687a5f291ac72a6086dc3eb3cbd0135f065
MuresanIA <muresan.alexandru933@gmail.com>
2020-05-16 10:54:29 +0300

    Added CoronaVirusDataService class.

---
+++ src/main/java/com/MIA/CoronvaVirusTracker/services/CoronaVirusDataService.java
@@ -0,0 +1,21 @@
+package com.MIA.CoronvaVirusTracker.services;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class CoronaVirusDataService {
+
+    private static String VIRUS_DATA_URLS = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+    public void fetchVirusData() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(VIRUS_DATA_URLS))
+                .build();
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(httpResponse.body());
+    }
+}
