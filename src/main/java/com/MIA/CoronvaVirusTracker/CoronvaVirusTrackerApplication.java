827f02635fd2fcef3724f4abd20347f71930a63a
MuresanIA <muresan.alexandru933@gmail.com>
2020-05-16 09:47:26 +0300

    first commit

---
+++ src/main/java/com/MIA/CoronvaVirusTracker/CoronvaVirusTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.MIA.CoronvaVirusTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronvaVirusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronvaVirusTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MIA/CoronvaVirusTracker/CoronvaVirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MIA.CoronvaVirusTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronvaVirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
