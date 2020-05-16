dd727b279db8eae5990b60207b68e589b9bfc61f
MuresanIA <muresan.alexandru933@gmail.com>
2020-05-16 14:05:47 +0300

    Added Controller and Models

---
+++ src/main/java/com/MIA/CoronvaVirusTracker/services/controllers/HomeController.java
@@ -0,0 +1,28 @@
+package com.MIA.CoronvaVirusTracker.services.controllers;
+
+import com.MIA.CoronvaVirusTracker.services.CoronaVirusDataService;
+import com.MIA.CoronvaVirusTracker.services.models.LocationStats;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int TotalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("TotalReportedCases", TotalReportedCases);
+
+        return "home";
+    }
+
+}
